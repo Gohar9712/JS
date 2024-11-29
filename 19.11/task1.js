885bfb91cbef6d67d16b0a08c79148386511686a
gohar_ter_stepanyan <gohar9970@gmail.com>
2024-11-29 21:02:53 +0400

    Sum_of_Elements

---
+++ 19.11/task1.js
@@ -0,0 +1,5 @@
+// Write a for loop to print the multiplication table of a given number n (e.g., n = 5).
+const n=5;
+for (let i = 1; i <= 10; i++) {
+    console.log(n +'* ' + i + ' = ' + n * i);
+}
